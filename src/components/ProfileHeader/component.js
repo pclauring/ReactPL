3aaae8e86d443e9ee83952fda2e0fcdcf35b9c53
Pierce Lauring <pclauring@gmail.com>
2018-04-21 18:43:00 -0400

    First Commit

---
+++ src/components/ProfileHeader/component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ProfileBanner extends React.Component {
+    render() {
+        return (<div className="ProfileBanner"></div>)
+    }
+}
