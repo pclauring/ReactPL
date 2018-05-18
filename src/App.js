d1ffeee0148adc29ff7571bbe1ae94e53a436d37
Pierce Lauring <pclauring@gmail.com>
2018-05-17 20:21:44 -0400

    Added a few new components to test node modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import ProfileHeader from './components/ProfileHeader/component.js';
+import BottomNavigation from './components/BottomNavigation/component.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
+import GithubButton from './components/GithubIcon/component.js';
+
 
 class App extends Component {
   render() {
@@ -10,9 +15,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Pierce Lauring</h1>
         </header>
+        <ProfileHeader />
         <p className="App-intro">
           Test Deploy Website
         </p>
+        <MuiThemeProvider>
+          <GithubButton />
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+        <BottomNavigation />
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/components/ProfileHeader/component.js
+++ src/components/ProfileHeader/component.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class ProfileBanner extends React.Component {
+class ProfileHeader extends React.Component {
     render() {
-        return (<div className="ProfileBanner"></div>)
+        return (<div className="ProfileHeader">Profile Header</div>)
     }
 }
+
+export default ProfileHeader;
